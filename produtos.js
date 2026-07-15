216d2bdef63077d79ad3adfe6e6c7cdf86045826
felipe1severo123-blip <felipe1severo123@gmail.com>
2026-07-15 14:12:57 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,3 +1,3 @@
 const bancoDeProdutos = [
-{ id: 1, titulo: "CD Celebridade - Nacional e Internacional (Vol. 1 e 2)", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 169,00", categoria: "disco", peso: 450, estado: "EE", capa: "fotos/prod-1-frente.jpg", verso: "fotos/prod-1-verso.jpg" },
+{ id: 1, titulo: "CD Celebridade - Nacional e Internacional (Vol. 1 e 2)", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 169,00", categoria: "disco", peso: 450, estado: "EE", capa: "fotos/prod-1-frente.jpeg", verso: "fotos/prod-1-verso.jpeg" },
 ];
