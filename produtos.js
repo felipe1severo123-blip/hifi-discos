2e95c81a6ba737159b71923536df851c61bae85e
felipe1severo123-blip <felipe1severo123@gmail.com>
2026-07-15 14:22:59 -0300

    s

--- produtos.js
+++ produtos.js
@@ -1,3 +1,8 @@
 const bancoDeProdutos = [
-{ id: 1, titulo: "CD Celebridade - Nacional e Internacional (Vol. 1 e 2)", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 169,00", categoria: "disco", peso: 450, estado: "EE", capa: "fotos/prod-1-frente.jpeg", verso: "fotos/prod-1-verso.jpeg" },
+{ id: 1, titulo: "CD Celebridade - Nacional e Internacional (Vol. 1 e 2)", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 169,00", categoria: "cd", peso: 450, estado: "EE", capa: "fotos/prod-1-frente.jpeg", verso: "fotos/prod-1-verso.jpeg" },
+{ id: 2, titulo: "CD O Melhor das Novelas Internacional Vol. 3", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2010", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-2-frente.jpeg", verso: "fotos/prod-2-verso.jpeg" },
+{ id: 3, titulo: "CD Canavial de Paixões - Trilha Sonora Original da Novela", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "203", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-3-frente.jpeg", verso: "fotos/prod-3-verso.jpeg" },
+
+
+
 ];
