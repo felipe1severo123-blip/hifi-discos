17beeda4ca6569fc4be17c028d57ecb9461378e8
felipe1severo123-blip <felipe1severo123@gmail.com>
2026-07-15 17:03:24 -0300

    s

--- produtos.js
+++ produtos.js
@@ -1,15 +1,15 @@
 const bancoDeProdutos = [
 { id: 1, titulo: "CD Celebridade - Nacional e Internacional (Vol. 1 e 2)", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 169,00", categoria: "cd", peso: 450, estado: "EE", capa: "fotos/prod-1-frente.jpeg", verso: "fotos/prod-1-verso.jpeg" },
 { id: 2, titulo: "CD O Melhor das Novelas Internacional Vol. 3", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2010", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-2-frente.jpeg", verso: "fotos/prod-2-verso.jpeg" },
-{ id: 3, titulo: "CD Canavial de Paixões - Trilha Sonora Original da Novela", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "203", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-3-frente.jpeg", verso: "fotos/prod-3-verso.jpeg" },
+{ id: 3, titulo: "CD Canavial de Paixões - Trilha Sonora Original da Novela", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2003", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-3-frente.jpeg", verso: "fotos/prod-3-verso.jpeg" },
 { id: 4, titulo: "CD Beleza Pura Internacional - Trilha Sonora da Novela", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "2008", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-4-frente.jpeg", verso: "fotos/prod-4-verso.jpeg" },
 { id: 5, titulo: "CD Quatro por Quatro Internacional - Trilha Sonora da Novela", artista: "Vários Artistas (Trilha Sonora da Novela da Rede Globo)", ano: "1995", preco: "R$ 149,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-5-frente.jpeg", verso: "fotos/prod-5-verso.jpeg" },
 { id: 6, titulo: "CD Jon Secada - Heart, Soul & a Voice", artista: "Jon Secada", ano: "1994", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-6-frente.jpeg", verso: "fotos/prod-6-verso.jpeg" },
-{ id: 7, titulo: "CD Jorge Aragão - Ao Vivo 2", artista: "Jorge Aragão", ano: "200", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-7-frente.jpeg", verso: "fotos/prod-7-verso.jpeg" },
-
-
-
-
+{ id: 7, titulo: "CD Jorge Aragão - Ao Vivo 2", artista: "Jorge Aragão", ano: "2000", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-7-frente.jpeg", verso: "fotos/prod-7-verso.jpeg" },
+{ id: 8, titulo: "CD Hanson - This Time Around", artista: "Hanson", ano: "2000", preco: "R$ 79,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-8-frente.jpeg", verso: "fotos/prod-8-verso.jpeg" },
+{ id: 9, titulo: "CD Legião Urbana - Mais do Mesmo (Os Maiores Sucessos)", artista: "Legião Urbana", ano: "1998", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-9-frente.jpeg", verso: "fotos/prod-9-verso.jpeg" },
+{ id: 10, titulo: "CD Passione Internacional - Trilha Sonora da Novela", artista: "Vários Artistas", ano: "2010", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-10-frente.jpeg", verso: "fotos/prod-10-verso.jpeg" },
+{ id: 11, titulo: "CD América Internacional - Trilha Sonora da Novela", artista: "Vários Artistas", ano: "2005", preco: "R$ 129,90", categoria: "cd", peso: 300, estado: "EE", capa: "fotos/prod-11-frente.jpeg", verso: "fotos/prod-11-verso.jpeg" },
 
 
 
